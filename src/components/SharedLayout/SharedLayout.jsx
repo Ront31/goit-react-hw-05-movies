8d0263d90dc380c192e05f50ae8650c79a30eb10
Ront31 <kolya.maslov.09@gmail.com>
2023-05-30 16:03:35 +0300

    finakl

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import { Loader } from '../Loader/Loader';
+import { Loader } from 'components/Loader/Loader';
 import css from './SharedLayout.module.css';
 import styled from 'styled-components';
 
@@ -25,7 +25,6 @@ export const SharedLayout = () => {
           </StyledLink>
         </nav>
       </header>
-      <Outlet />
       <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
